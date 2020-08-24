83cb54172ba1a274ba56edf0400ac6e7e72c1de8
Victor Carvalho <victorcarvalhosp@gmail.com>
2020-08-24 10:02:45 -0300

    Remove unused routes

--- routes/routes.ts
+++ routes/routes.ts
@@ -21,39 +21,5 @@ export class Routes {
         .then((businesses: Array<Business>) => res.json(businesses))
         .catch((err: Error) => res.status(500).json(err))
     );
-
-    app.route("/businesses").post((req, res) =>
-      this.businessesController
-        .create(req.body)
-        .then((business: Business) => res.status(201).json(business))
-        .catch((err: Error) => res.status(500).json(err))
-    );
-
-    app.route("/businesses/:id").get((req, res) =>
-      this.businessesController
-        .show(Number.parseInt(req.params.id))
-        .then((business: Business | null) => {
-          if (business) {
-            res.json(business);
-          } else {
-            res.status(404).json({ errors: ["Business not found"] });
-          }
-        })
-        .catch((err: Error) => res.status(500).json(err))
-    );
-
-    app.route("/businesses/:id").put((req, res) =>
-      this.businessesController
-        .update(Number.parseInt(req.params.id), res.body)
-        .then(() => res.status(202).json({ data: "success" }))
-        .catch((err: Error) => res.status(500).json(err))
-    );
-
-    app.route("/businesses/:id").delete((req, res) =>
-      this.businessesController
-        .delete(Number.parseInt(req.params.id))
-        .then(() => res.status(204).json({ data: "success" }))
-        .catch((err: Error) => res.status(500).json(err))
-    );
   }
 }
